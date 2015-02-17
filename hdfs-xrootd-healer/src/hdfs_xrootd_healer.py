1a7b7dec26fb065d49c697e0d089496e9472afc4
Jeff Dost <jdost@ucsd.edu>
2015-02-16 22:26:48 -0800

    add config parsing ability

--- hdfs-xrootd-healer/src/hdfs_xrootd_healer.py
+++ hdfs-xrootd-healer/src/hdfs_xrootd_healer.py
@@ -7,7 +7,7 @@ import md5
 
 import subprocess
 
-NAMESPACE = '/cms/phedex/store/relval'
+'''NAMESPACE = '/cms/phedex/store/relval'
 if len(sys.argv) > 1:
   NAMESPACE = sys.argv[1]
 LOGICAL_DIR = '/cms/phedex'
@@ -17,6 +17,43 @@ CKSUM_DIR = '/cksums'
 # 128 mb
 BLOCK_SIZE = 134217728
 HDFS_TMP_DIR = '/hdfshealer'
+'''
+
+def parse_conf(path):
+  d = {
+    'NAMESPACE': '',
+    'LOGICAL_DIR': '',
+    'CACHE_DIR': '/cksums',
+    'FUSE_MOUNT': '',
+    'CKSUM_DIR': '',
+    'HDFS_TMP_DIR': '',
+    'BLOCK_SIZE': 134217728
+  }
+
+  line_num = 1
+  f = open(path)
+  try:
+    for line in f:
+      line = line.split('#')[0]
+      line_arr = line.split('=')
+      if len(line_arr) >= 2:
+        key = line_arr[0].strip()
+        if key.upper() not in d:
+          raise ValueError('%s: line %s: %s' % (path, line_num, key))
+        key = key.upper()
+        if key == 'BLOCK_SIZE':
+          d[key] = int(line_arr[1].strip())
+        else:
+          d[key] = line_arr[1].strip()
+      line_num += 1
+  finally:
+    f.close()
+  return d
+
+if len(sys.argv) > 1:
+  CONF_PATH = sys.argv[1]
+
+CONF = parse_conf(CONF_PATH)
 
 def build_cache_set(path, cached_files):
   if os.path.isdir(path):
@@ -27,19 +64,19 @@ def build_cache_set(path, cached_files):
       build_cache_set(abs_path, cached_files)
   else:
     if path.endswith('.cinfo'):
-      cached_files.add(re.sub(r'___[0-9]+_[0-9]+.cinfo$', '', path).replace(CACHE_DIR, LOGICAL_DIR, 1))
+      cached_files.add(re.sub(r'___[0-9]+_[0-9]+.cinfo$', '', path).replace(CONF['CACHE_DIR'], CONF['LOGICAL_DIR'], 1))
 
 cached_files = set()
-build_cache_set(NAMESPACE.replace(LOGICAL_DIR, CACHE_DIR, 1), cached_files)
+build_cache_set(CONF['NAMESPACE'].replace(CONF['LOGICAL_DIR'], CONF['CACHE_DIR'], 1), cached_files)
 
 # hack to make single namespace work for testing
 # it must be a full cinfo cache path to work
-if not os.path.isdir(NAMESPACE):
-  NAMESPACE = re.sub(r'___[0-9]+_[0-9]+.cinfo$', '', NAMESPACE)
+if not os.path.isdir(CONF['NAMESPACE']):
+  CONF['NAMESPACE'] = re.sub(r'___[0-9]+_[0-9]+.cinfo$', '', CONF['NAMESPACE'])
 
 broken_files = []
 
-p = subprocess.Popen(['hdfs', 'fsck', NAMESPACE], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+p = subprocess.Popen(['hdfs', 'fsck', CONF['NAMESPACE']], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 for line in p.stdout:
   #print line,
@@ -58,9 +95,10 @@ p.wait()
 
 for f in broken_files:
   if f in cached_files:
-    orig_md5_path = '%s%s%s' % (FUSE_MOUNT, CKSUM_DIR, f)
+    orig_md5_path = '%s%s%s' % (CONF['FUSE_MOUNT'], CONF['CKSUM_DIR'], f)
+
+    fin = open(orig_md5_path) 
     try:
-      fin = open(orig_md5_path) 
       for line in fin:
         if line.startswith('MD5:'):
           orig_md5 = line.split(':')[1].strip()
@@ -71,19 +109,19 @@ for f in broken_files:
 
     new_md5 = md5.new()
 
-    orig_filepath = '%s%s' % (FUSE_MOUNT, f)
+    orig_filepath = '%s%s' % (CONF['FUSE_MOUNT'], f)
 
     orig_stat = os.stat(orig_filepath)
 
     f_base = os.path.basename(f)
-    tmp_filepath = os.path.join('%s%s' % (FUSE_MOUNT, HDFS_TMP_DIR), f_base)
-    try:
-      fin = open(orig_filepath, 'rb')
-      print tmp_filepath
-      fout = open(tmp_filepath, 'wb')
+    tmp_filepath = os.path.join('%s%s' % (CONF['FUSE_MOUNT'], CONF['HDFS_TMP_DIR']), f_base)
 
+    fin = open(orig_filepath, 'rb')
+    print tmp_filepath
+    fout = open(tmp_filepath, 'wb')
+    try:
       while True:
-        bytes = fin.read(BLOCK_SIZE)
+        bytes = fin.read(CONF['BLOCK_SIZE'])
         if bytes == '':
           break
         new_md5.update(bytes)
